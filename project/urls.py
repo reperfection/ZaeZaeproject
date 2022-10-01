37335fc35f7d79d46a562dce226eb81ebcd83bf1
Jaewan, 再完 <wodhkszxc@gmail.com>
2022-10-01 15:56:32 +0900

    Add modify page, an noneditable field

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 import posts.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,6 @@ urlpatterns = [
     path('create/', posts.views.create, name='create'),
     path('read/', posts.views.read, name='read'),
     path('detail/<str:id>', posts.views.detail, name='detail'),
-]
+    path('edit/<str:id>/', posts.views.edit, name='edit'),
+    path('delete/<str:id>/', posts.views.delete, name='delete'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
